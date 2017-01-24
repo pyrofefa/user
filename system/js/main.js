dc5313f14d93c1c27695e353fb1545979a8ab165
Esteban Flores <esteban@teknol.com.mx>
2017-01-23 17:21:06 -0700

    Cambios despues del robo

--- system/js/main.js
+++ system/js/main.js
@@ -12,8 +12,19 @@ rutas.config(function($routeProvider) {
             templateUrl : 'vistas/inicio.html',
             controller  : 'inicioController'
         })
+        .when('/seleccionar', {
+            templateUrl : 'vistas/seleccionar.html'
+        })
+        .when('/seleccionar/pagos',{
+            templateUrl : 'vistas/pagos.html',
+            controller  : 'pagosController'  
+        })
+        .when('/seleccionar/aclaraciones',{
+            templateUrl : 'vistas/aclaraciones.html',
+            controller  : 'aclaracionesController'  
+        })
         .when('/empezar', {
-            templateUrl : 'vistas/empezar.html',
+            templateUrl : 'vistas/pagos.html',
             controller  : 'empezarController'
         })
         /*final*/  
@@ -24,7 +35,7 @@ rutas.config(function($routeProvider) {
 
 //lamando a socket en angular
 rutas.factory('socket',['$rootScope', function($rootScope){
-    var socket = io.connect('http://localhost:8080');
+    var socket = io.connect('http://192.168.2.133:8080');
     return{
         on: function(eventName, callback){
             socket.on(eventName, callback);
@@ -37,8 +48,6 @@ rutas.factory('socket',['$rootScope', function($rootScope){
 //controlador 
 rutas.controller('inicioController', function($scope, $http, $route, socket) 
 {
-    
-    /*Fin de aclaraciones */
     socket.on('turno',function(data){
         console.log(data);
         $scope.$apply(function(){
@@ -51,21 +60,33 @@ rutas.controller('inicioController', function($scope, $http, $route, socket)
             $scope.caja = data;
         });
     })
+    socket.on('asunto',function(data){
+        console.log(data);
+        $scope.$apply(function(){
+            $scope.asunto = data;
+        });
+    })
+
     $scope.iniciar_sesion = function()
     {
         var caja=$scope.caja;
         var contra=$scope.password;
+        var sucursal=$scope.sucursal;
+        console.log(sucursal);
         $http({
             method:"post",
-            url: "http://agua.dev/user/iniciar",
+            url: "http://localhost/admin_agua/public/user/iniciar/"+sucursal,
             data: ({'name' : caja , 'password' :  contra })
         }).success(function(data){
-            console.log(data);
+            //console.log(data);
             if(data==1)
             {
-                window.location.href="#/empezar";
+                window.location.href="#/seleccionar";
                 localStorage.caja = caja;
-                //console.log("Local storage: "+localStorage.caja);
+                document.getElementById("caja").innerHTML = localStorage.caja;
+                localStorage.sucursal= sucursal;
+                console.log("Local storage caja: "+localStorage.caja);
+                console.log("Local storage caja: "+localStorage.sucursal);
             }
             else if(data==0)
             {
@@ -78,30 +99,43 @@ rutas.controller('inicioController', function($scope, $http, $route, socket)
     }
 
 });
-rutas.controller('empezarController', function($scope, $http, $route, socket){
+rutas.controller('pagosController', function($scope, $http, $route, socket){
+    
+    $scope.abandonado = true;
+    $scope.terminar = true;
+    $scope.mostrar = "Turno en espera"
 
     $http({
         method:"get",
-        url: "http://agua.dev/home/mostrar"
+        url: "http://localhost/admin_agua/public/home/mostrarpagos/"+localStorage.sucursal
         }).success(function(data){
+            console.log(data.turno);
             $scope.datos=data;
-            console.log(data);
         }).error(function(data){
             alert("Ha ocurrido un error al mostrar los datos");
     });
 
-    $scope.tomar_turno=function($id, $turno)
+    $scope.tomar_turno=function($id, $turno, $subasunto)
     {
-
+        //alert("haz hecho click  id:"+ $id+" turno: "+$turno);
         socket.emit('caja', localStorage.caja);
         socket.emit('turno',$turno);
-        
+        socket.emit('asunto',$subasunto);
+        socket.emit('tipo','CAJA')
+
+        $scope.tomar=true;
+        $scope.volver=true;
+
         $http({
             method:"put",
-            url: "http://agua.dev/tikets/actualizar/"+$id,
-            data: ({ 'estado' : 1, 'fk_caja' : localStorage.caja})
+            url: "http://localhost/admin_agua/public/tikets/actualizar/"+$id,
+            data: ({ 'id_sucursal' : localStorage.sucursal, 'estado' : 1, 'fk_caja' : localStorage.caja})
         }).success(function(data){
             //console.log(data);
+            $scope.tomar = true;
+            $scope.abandonado = false;
+            $scope.terminar = false;
+            $scope.mostrar = "Atendiendo"
             //$route.reload();
         }).error(function(data){
             //console.log(data);
@@ -138,33 +172,16 @@ rutas.controller('empezarController', function($scope, $http, $route, socket){
             $("#noc").val(hora + ":" + minuto + ":"+ segundos);
 
         }, 1000);
-    } 
-    $scope.turno_abandonado = function($id, $turno)
+    }
+    $scope.terminar_turno = function($id, $turno)
     {
-        $http({
-            method:"put",
-            url: "http://agua.dev/tikets/actualizar/"+$id,
-            data: ({ 'estado' : 2, 'fk_caja' : localStorage.caja})
-        }).success(function(data){
-            //console.log(data);
-            $route.reload();
-        }).error(function(data){
-            //console.log(data);
-            alert("Ha ocurrido un error al actualizar los datos");
-            $route.reload();
-            //console.log(id);
-        })
-    };
-    $scope.terminar = function($id, $turno)
-    {
-        
+        //alert("Terminado");
         var tiempo = $("#noc").val();
-
         console.log(tiempo);
 
         $http({
             method:"put",
-            url: "http://agua.dev/tikets/actualizar/"+$id,
+            url: "http://localhost/admin_agua/public/tikets/actualizar/"+$id,
             data: ({ 'tiempo' : tiempo})
         }).success(function(data){
             //console.log(data);
@@ -172,12 +189,153 @@ rutas.controller('empezarController', function($scope, $http, $route, socket){
         }).error(function(data){
             //console.log(data);
             alert("Ha ocurrido un error al actualizar los datos");
+            //$route.reload();
+            //console.log(id);
+        })
+    }
+    $scope.turno_abandonado = function($id, $turno)
+    {
+        //alert("Abandonado");
+        var tiempo = $("#noc").val();
+        console.log(tiempo);
+        
+        $http({
+            method:"put",
+            url: "http://localhost/admin_agua/public/tikets/actualizar/"+$id,
+            data: ({ 'estado' : 2, 'fk_caja' : localStorage.caja,'tiempo' : tiempo })
+        }).success(function(data){
+            //console.log(data);
+            $route.reload();
+        }).error(function(data){
+            //console.log(data);
+            alert("Ha ocurrido un error al actualizar los datos");
             $route.reload();
             //console.log(id);
         })
-    } 
-    $scope.refresh = function()
+    }
+    $scope.volver_atras = function()
     {
-        $route.reload();
-    }    
+        window.location.href = "#/seleccionar";
+    }
+});
+rutas.controller('aclaracionesController', function($scope, $http, $route, socket){
+    
+    $scope.abandonado = true;
+    $scope.terminar = true;
+    $scope.mostrar = "Turno en espera"
+
+    $http({
+        method:"get",
+        url: "http://localhost/admin_agua/public/home/mostraraclaraciones/"+localStorage.sucursal
+        }).success(function(data){
+            $scope.datos=data;
+            console.log(data);
+        }).error(function(data){
+            alert("Ha ocurrido un error al mostrar los datos");
+    });
+
+    $scope.tomar_turno=function($id, $turno, $subasunto)
+    {
+        //alert("haz hecho click  id:"+ $id+" turno: "+$turno);
+        socket.emit('caja', localStorage.caja);
+        socket.emit('turno',$turno);
+        socket.emit('asunto',$subasunto);
+        socket.emit('tipo','MODULO')
+
+        $scope.tomar=true;
+        $scope.volver = true;
+
+        $http({
+            method:"put",
+            url: "http://localhost/admin_agua/public/tikets/actualizar/"+$id,
+            data: ({ 'estado' : 1, 'fk_caja' : localStorage.caja})
+        }).success(function(data){
+            //console.log(data);
+            $scope.tomar = true;
+            $scope.abandonado = false;
+            $scope.terminar = false;
+            $scope.mostrar = "Atendiendo"
+            //$route.reload();
+        }).error(function(data){
+            //console.log(data);
+            alert("Ha ocurrido un error al actualizar los datos");
+            //$route.reload();
+            //console.log(id);
+        })
+
+         var tiempo = {
+            hora: 0,
+            minuto: 0,
+            segundo: 0
+        };
+
+        var tiempo_corriendo = null;
+        tiempo_corriendo = setInterval(function()
+        {
+            // Segundos
+            tiempo.segundo++;
+            if(tiempo.segundo >= 60)
+            {
+                tiempo.segundo = 0;
+                tiempo.minuto++;
+            }      
+            // Minutos
+            if(tiempo.minuto >= 60)
+            {
+                tiempo.minuto = 0;
+                tiempo.hora++;
+            }
+            hora = tiempo.hora < 10 ? '0' + tiempo.hora : tiempo.hora;
+            minuto = tiempo.minuto < 10 ? '0' + tiempo.minuto : tiempo.minuto;
+            segundos = tiempo.segundo < 10 ? '0' + tiempo.segundo : tiempo.segundo;
+            $("#noc").val(hora + ":" + minuto + ":"+ segundos);
+
+        }, 1000);
+    }
+    $scope.terminar_turno = function($id, $turno)
+    {
+        //alert("Terminado");
+        var tiempo = $("#noc").val();
+       //console.log (tiempo);
+
+        $http({
+            method:"put",
+            url: "http://localhost/admin_agua/public/tikets/actualizar/"+$id,
+            data: ({ 'tiempo' : tiempo})
+        }).success(function(data){
+            //console.log(data);
+            $route.reload();
+        }).error(function(data){
+            //console.log(data);
+            alert("Ha ocurrido un error al actualizar los datos");
+            //$route.reload();
+            //console.log(id);
+        })
+    }
+    $scope.turno_abandonado = function($id, $turno)
+    {
+        //alert("Abandonado");
+        var tiempo = $("#noc").val();
+        //console.log(tiempo);
+        
+        $http({
+            method:"put",
+            url: "http://localhost/admin_agua/public/tikets/actualizar/"+$id,
+            data: ({ 'estado' : 2, 'fk_caja' : localStorage.caja,'tiempo' : tiempo })
+        }).success(function(data){
+            //console.log(data);
+            $route.reload();
+        }).error(function(data){
+            //console.log(data);
+            alert("Ha ocurrido un error al actualizar los datos");
+            $route.reload();
+            //console.log(id);
+        })
+    }
+    $scope.volver_atras = function()
+    {
+        window.location.href = "#/seleccionar";
+    }
+
+
 });
